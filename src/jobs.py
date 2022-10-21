c4630ecce109df0e9b6d9b475769e9e9caf245a6
Lucas5k <lucascabral112347@gmail.com>
2022-10-21 15:25:49 -0300

    feat: req 1, criação da função de leitura feita com sucesso

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,12 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
-def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+def read(path: str):
+    with open(path, encoding="utf8") as file:
+        all_file = csv.DictReader(file, delimiter=',', quotechar='"')
+        my_list = []
+        for value in all_file:
+            my_list.append(value)
+    return my_list
