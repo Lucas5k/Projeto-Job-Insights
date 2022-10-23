a3420a25f9d30fda2f1b2c450047d5071ddd8be3
Lucas5k <lucascabral112347@gmail.com>
2022-10-22 21:28:18 -0300

    feat: implementação do req 10 concluido com sucesso

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    count = count_ocurrences('src/jobs.csv', 'a')
+    assert count == 705686
