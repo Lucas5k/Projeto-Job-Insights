66e7c964dfb668c9fb082475ec9d1d62c2f2656c
Lucas5k <lucascabral112347@gmail.com>
2022-11-06 20:03:50 -0300

    test

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,7 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    results = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    for result in results:
+        assert result['title'] is True
